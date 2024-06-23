cf329c50c257554f7a362a71d846c0b1a6df732e
alancriaxyz <606570+alancriaxyz@users.noreply.github.com>
2024-06-23 02:35:10 -0300

    Add analytics token in .env

--- src/lib/components/analytics/beam.svelte
+++ src/lib/components/analytics/beam.svelte
@@ -1,15 +1,16 @@
 <script>
-	import { onMount } from 'svelte';
+  import { onMount } from 'svelte'
 
-	let isProduction = import.meta.env.VITE_ENV === 'production';
+  let isProduction = import.meta.env.VITE_ENV === 'production'
+  let beamToken = import.meta.env.BEAM_ANALYTICS_TOKEN
 
-	onMount(() => {
-		if (isProduction) {
-			const script = document.createElement('script');
-			script.src = 'https://beamanalytics.b-cdn.net/beam.min.js';
-			script.setAttribute('data-token', '5ef93360-2cba-4abc-841b-cfabe3abcff7');
-			script.async = true;
-			document.head.appendChild(script);
-		}
-	});
+  onMount(() => {
+    if (isProduction) {
+      const script = document.createElement('script')
+      script.src = 'https://beamanalytics.b-cdn.net/beam.min.js'
+      script.setAttribute('data-token', beamToken)
+      script.async = true
+      document.head.appendChild(script)
+    }
+  })
 </script>
