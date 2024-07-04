e2fb31b37f7cd419bd75b64637dc51fdd74ec06c
alancriaxyz <606570+alancriaxyz@users.noreply.github.com>
2024-07-04 01:19:54 -0300

    📝 (posts.svelte): update styling classes for post title and description in Card component to improve readability and consistency

--- src/lib/components/posts/posts.svelte
+++ src/lib/components/posts/posts.svelte
@@ -11,10 +11,12 @@
       <Card.Root
         class="overflow-hidden rounded-lg transition-all duration-500 hover:shadow-[0_0_5px_rgba(221,221,221,0.5),0_0_15px_rgba(204,204,204,0.5)]">
         <Card.Header>
-          <Card.Title title={post.title}>{post.title}</Card.Title>
-          <Card.Description>{post.description}</Card.Description>
+          <Card.Title title={post.title} class="text-gray-950 dark:text-gray-50"
+            >{post.title}</Card.Title>
+          <Card.Description class="text-gray-600 dark:text-gray-300"
+            >{post.description}</Card.Description>
         </Card.Header>
-        <Card.Footer class="text-xs text-gray-100">
+        <Card.Footer class="text-xs text-gray-500 dark:text-gray-400">
           {post.timeAgo}
         </Card.Footer>
       </Card.Root>
