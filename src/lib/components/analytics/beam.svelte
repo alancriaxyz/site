bd2a8503861f1850a196bf49b3d51992a5c6318e
alancriaxyz <606570+alancriaxyz@users.noreply.github.com>
2024-07-04 23:45:36 -0300

    ♻️ (beam.svelte): refactor analytics script insertion to remove unnecessary meta tag and improve code readability

--- src/lib/components/analytics/beam.svelte
+++ src/lib/components/analytics/beam.svelte
@@ -6,12 +6,6 @@
 
   onMount(() => {
     if (isProduction) {
-      const meta = document.createElement('meta')
-      meta.httpEquiv = 'Content-Security-Policy'
-      meta.content =
-        "script-src 'self' https://beamanalytics.b-cdn.net; object-src 'none';"
-      document.head.appendChild(meta)
-
       const script = document.createElement('script')
       script.src = 'https://beamanalytics.b-cdn.net/beam.min.js'
       script.setAttribute('data-token', beamToken)
