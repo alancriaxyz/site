c8960dc517f3748846727716fa9ebe23faf9eccf
alancriaxyz <606570+alancriaxyz@users.noreply.github.com>
2024-07-04 22:47:29 -0300

    🐛 (beam.svelte): fix setting data-token attribute to use isProduction flag instead of beamToken for better logic handling

--- src/lib/components/analytics/beam.svelte
+++ src/lib/components/analytics/beam.svelte
@@ -8,7 +8,7 @@
     if (isProduction) {
       const script = document.createElement('script')
       script.src = 'https://beamanalytics.b-cdn.net/beam.min.js'
-      script.setAttribute('data-token', beamToken)
+      script.setAttribute('data-token', isProduction)
       script.async = true
       document.head.appendChild(script)
     }
